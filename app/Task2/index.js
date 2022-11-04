9cf590dc21771127bf1d4b0b9435c6be30ef041c
yurchenko08 <yurchenko08@gmail.com>
2022-11-04 21:51:29 +0200

    finished task2

--- app/Task2/index.js
+++ app/Task2/index.js
@@ -1,29 +1,55 @@
 import React, { Component } from "react";
 
 export default class Task2 extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            list: [
-                {
-                    id: 1,
-                    text: "hidden text 1"
-                },
-                {
-                    id: 2,
-                    text: "hidden text 2"
-                },
-                {
-                    id: 3,
-                    text: "hidden text 3"
-                },
-            ],
-            text: ''
-        };
-    }
+    this.state = {
+      list: [
+        {
+          id: 1,
+          text: "hidden text 1",
+        },
+        {
+          id: 2,
+          text: "hidden text 2",
+        },
+        {
+          id: 3,
+          text: "hidden text 3",
+        },
+      ],
+      text: "",
+    };
+  }
+  onMouseOver = (text) => {
+    this.setState({
+      text: text,
+    });
+  };
 
-    render() {
-        return null;
-    }
+  onMouseOut = () => {
+    this.setState({
+      text: "",
+    });
+  };
+
+  render() {
+    const { list, text } = this.state;
+    return (
+      <div>
+        {list.map((el) => (
+          <div
+            key={el.id}
+            className='element'
+            onMouseOver={() => this.onMouseOver(el.text)}
+            onMouseOut={this.onMouseOut}
+          >
+            id - {el.id}
+          </div>
+        ))}
+        <div className='text'>{text}</div>
+      </div>
+    );
+  }
 }
